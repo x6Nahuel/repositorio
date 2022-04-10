b95107761eff5b7ba2aebf282644c763f493e7f9
PC <PC@DESKTOP-TVIBC5F>
2022-04-10 19:14:52 -0300

    libros

---
+++ ProjectDemo/src/main/java/ar/edu/unju/fi/pvisual/ejemplos/aplicacion/ProjectDemoApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.pvisual.ejemplos.aplicacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectDemoApplication.class, args);
+	}
+
+}
---
+++ ProjectDemo/src/test/java/ar/edu/unju/fi/pvisual/ejemplos/aplicacion/ProjectDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.pvisual.ejemplos.aplicacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
